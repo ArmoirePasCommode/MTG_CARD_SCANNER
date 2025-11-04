1c1da3663a4d0c1f0dbe377655d3b5b08e74ec75
ArmoirePasCommode <twittel.canada@gmail.com>
2025-11-04 14:00:17 +0100

    PUTAING CONG

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,24 @@ export default {
   moduleFileExtensions: ['ts', 'js'],
   verbose: true,
   setupFiles: ['<rootDir>/tests/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          module: 'NodeNext',
+          moduleResolution: 'NodeNext',
+          isolatedModules: true
+        },
+        diagnostics: { ignoreCodes: [151002] }
+      }
+    ]
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
 };
 
 
